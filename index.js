9bbe5fa661075e24bd5037ab3e4d735fee82b574
satya951 <satmakuri225@gmail.com>
2023-05-12 16:51:36 +0200

    removed git com from js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/satya951/tic_tac_toe.git
-git push -u origin main
+console.log("satya");
